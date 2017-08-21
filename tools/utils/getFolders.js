db5e495bb8b8553db6fa28decf752a5564bb9768
Second To None <secondtonone.secondtonone@gmail.com>
2017-08-21 17:00:13 +0300

    Example improve

--- tools/utils/getFolders.js
+++ tools/utils/getFolders.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = function (dir) {
     return fs.readdirSync(dir)
-      .filter(file => {
-        return fs.statSync(path.join(dir, file)).isDirectory();
-      });
+        .filter(file => {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
 };
