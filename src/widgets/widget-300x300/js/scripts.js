42dacf7a0c458d8309ba69636560cd47b6306113
Second To None <secondtonone.secondtonone@gmail.com>
2017-08-23 17:41:00 +0300

    Small and middle widget

--- src/widgets/widget-300x300/js/scripts.js
+++ src/widgets/widget-300x300/js/scripts.js
@@ -1,15 +1,26 @@
 import { getParametersValues, makeLinkCheckout} from '../../../utils';
 
 
-var enterWidgetParams = ['public_key'];
+var enterWidgetParams = ['public_key', 'title', 'button_name'];
 
 var widgetParams = getParametersValues(enterWidgetParams);
 
-
-
 var input = document.getElementById('donation-amount');
 
-document.getElementById('make-donation').addEventListener('click', function() {
+
+
+var title = document.getElementById('merchant-title');
+
+title.innerHTML = widgetParams['title'] || '';
+
+
+var button = document.getElementById('make-donation');
+
+button.innerHTML = widgetParams['button_name'] || 'Помочь сейчас';
+
+
+
+button.addEventListener('click', function() {
 
     var checkoutParams = {
         public_key: widgetParams['public_key'],
---
+++ src/widgets/widget-button-300x180/js/scripts.js
@@ -0,0 +1,27 @@
+import {getParametersValues, makeLinkCheckout} from '../../utils';
+
+var enterWidgetParams = ['public_key', 'button_name', 'title'];
+
+var widgetParams = getParametersValues(enterWidgetParams);
+
+
+
+var button = document.getElementById('make-donation');
+
+button.innerHTML = widgetParams['button_name'] || 'Оплатить';
+
+
+var input = document.getElementById('merchant-title');
+
+input.innerHTML = widgetParams['title'] || '';
+
+
+
+button.addEventListener('click', function() {
+
+    var checkoutParams = {
+        public_key: widgetParams['public_key']
+    };
+
+    parent.location.href = makeLinkCheckout(checkoutParams);
+});
