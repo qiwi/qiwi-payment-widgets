173900c72af915cc9b70cc6db07cb140a39312b4
q-github-opensource <github-opensource@qiwi.com>
2017-09-21 16:05:00 +0300

    Automatic updates from Open-Source/qiwi-payment-widgets, branch master

---
+++ src/widgets/middle-widget-300x270/main.js
@@ -0,0 +1,18 @@
+import './css/styles.css';
+
+import WidgetInput from '../../components/widget-input';
+
+const middleWidget300x270 = new WidgetInput(['public_key', 'button_name']);
+
+middleWidget300x270.init({
+    button: {
+        id: 'make-donation'
+    },
+    message: {
+        id: 'error-message'
+    },
+    input: {
+        id: 'donation-amount',
+        errorState: 'widget__field--error'
+    }
+});
