7457dc6e9d8d206fc73286d4baac8586f5e6ed78
q-github-opensource <github-opensource@qiwi.com>
2018-01-26 15:51:42 +0300

    Automatic updates from Open-Source/qiwi-payment-widgets, branch master

--- src/widgets/middle-button-200x80/main.js
+++ src/widgets/middle-button-200x80/main.js
@@ -1,11 +1,10 @@
 import './css/styles.css';
-import WidgetButton from'../../components/widget-button';
-
+import WidgetButton from '../../components/widget-button';
 
 const middleButton200x80 = new WidgetButton();
 
 middleButton200x80.init({
-    button:{
-        id:'make-donation'
+    redirect: {
+        id: 'make-donation'
     }
 });
