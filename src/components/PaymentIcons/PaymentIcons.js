06af0fcc8962b56c5894964b2052faa513fb495f
Evgenii <magicwolf9@mail.ru>
2018-07-03 19:33:10 +0300

    Added change berween 2 variants of paymentIcons on different background colors

--- src/components/PaymentIcons/PaymentIcons.js
+++ src/components/PaymentIcons/PaymentIcons.js
@@ -1,4 +1,5 @@
 import './style.css';
+import {getContrastColorByBackground, styleCode, color} from '../../modules/helpers.js';
 
 export default function PaymentIcons () {
     const paymentIcons = document.createElement('div');
@@ -7,11 +8,33 @@ export default function PaymentIcons () {
 
     paymentIcons.innerHTML = `
             <div class="widget__payment-type widget__payment-block-visa"></div>
-            <div class="widget__payment-type widget__payment-block-master-card"></div>
+            <div class="widget__payment-type widget__payment-block-mastercard"></div>
             <div class="widget__payment-type widget__payment-block-mir"></div>
             <div class="widget__payment-type widget__payment-block-qiwi"></div>`;
 
-    return {
-        element: paymentIcons
+    const component = {
+        element: paymentIcons,
+        init: (data) => {
+            if (data.merchant_styles[styleCode.WIDGET_BACKGROUND]) {
+                component.setBackground(data.merchant_styles[styleCode.WIDGET_BACKGROUND]);
+            } else {
+                component.setBackground(color.WHITE);
+            }
+        },
+        setBackground: (widgetBackground) => {
+            const contrastColor = getContrastColorByBackground(widgetBackground);
+            const bgIndexInStyle = contrastColor === color.WHITE ? 0 : 1;
+            const paymentMethods = component.element.querySelectorAll('[class*=widget__payment-block-]');
+
+            paymentMethods.forEach(function (paymentMethod) {
+                const backgrounds = window.getComputedStyle(paymentMethod).getPropertyValue('background-image').split(', url(');
+                if (bgIndexInStyle === 1) {
+                    backgrounds[bgIndexInStyle] = `url(${backgrounds[bgIndexInStyle]}`;
+                }
+                paymentMethod.style.backgroundImage = backgrounds[bgIndexInStyle];
+            })
+        }
     };
+
+    return component;
 }
